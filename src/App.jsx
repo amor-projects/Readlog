4b459ef9c8376d52103fd9221db32d33cbc8cdf9
Amor Zephyr <amorzephyr@gmail.com>
2026-03-14 13:10:02 +0500

    Add Outlet to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from 'react';
+import { Outlet } from 'react-router';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Readlog</h1>
+      <Outlet />
     </>
   )
 }
